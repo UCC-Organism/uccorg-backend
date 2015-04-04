65f3bac11080ebf102a70b3e6c0ce7023c5a3601
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-04-04 11:55:26 +0200

    configuration data for test

---
+++ data-test/behaviour.js
@@ -0,0 +1,70 @@
+var config = {
+    "calendar": require("./calendar.js"),
+    "locations": require("./locations.json"),
+    "agents": require("./agents.json"),
+};
+
+var randomSeed;
+
+exports.calendarAgents = function(calendar, uccorg, data) {
+    var id, i, j, o, researchers, activity, activities;
+    randomSeed = 0;
+
+    for (var agentType in config.agents) {
+        var agentInfo = config.agents[agentType];
+        if (agentInfo.count) {
+            var agentIds = agentNames(agentType, agentInfo.count);
+            for (i = 0; i < agentIds.length; ++i) {
+                uccorg.addAgent({
+                    id: agentIds[i],
+                    kind: agentType,
+                    age: agentInfo.minAge + pseudoRandom() * (agentInfo.maxAge - agentInfo.minAge) | 0,
+                    gender: (pseudoRandom() > agentInfo.genderBalance) ? 0 : 1
+                });
+            }
+        } else {
+            uccorg.addAgent({
+                id: agentType,
+                kind: agentType
+            });
+        }
+    }
+
+    for (i = 0; i < calendar.length; ++i) {
+        o = calendar[i];
+        activities = config.calendar[o.type];
+        if (activities) {
+            for (j = 0; j < activities.length; ++j) {
+                activity = activities[j];
+                if (activity.frequencyPerHour) {
+                    uccorg.randomEvents(o.start, o.end.slice(0, 19), [activity]);
+                } else {
+                    uccorg.addEvent({
+                        time: o.start,
+                        likelyEndTime: o.end.slice(0, 19),
+                        agents: activity.agents,
+                        location: activity.location,
+                        description: activity.activity || o.type
+                    });
+                    uccorg.addEvent({
+                        time: (new Date(new Date(o.end.slice(0, 19) + 'Z') - 1000)).toISOString().slice(0, 19),
+                        agents: activity.agents
+                    });
+                }
+            }
+        }
+    }
+};
+
+function pseudoRandom() {
+    randomSeed = 1103515245 * randomSeed + 12345 & 0x7fffffff;
+    return (randomSeed & 0x3fffffff) / 0x40000000;
+}
+
+function agentNames(name, count) {
+    var result = [];
+    for (var i = 1; i <= count; ++i) {
+        result.push(name + i);
+    }
+    return result;
+}
---
+++ data-test/calendar.js
@@ -0,0 +1,69 @@
+module.exports = {
+  "cooking": [{
+    "agents": agentNames("kitchen staff", 10),
+    "location": ["cafe", "canteen", "kitchen", "kitchen", "kitchen", "canteen"]
+  }],
+  "workers": [{
+    "agents": agentNames("researcher", 10),
+    "activity": "researchers",
+    "location": ["B.103", "B.105", "B.104", "B.109", "A.104", "A.106"]
+  }, {
+    "agents": agentNames("administrator", 10),
+    "activity": "administrators",
+    "location": ["C.103", "C.105", "C.104", "C.109", "B.104", "B.106"]
+  }],
+  "janitors": [{
+    "agents": agentNames("janitor", 2),
+    "activity": "roaming"
+  }, {
+    "agentTypes": ["janitor"],
+    "minDuration": 1,
+    "maxDuration": 3,
+    "description": "toilet",
+    "locations": ["random toilet"],
+    "during": ["scheduled", "roaming"],
+    "frequencyPerHour": 0.1
+  },{
+    "agentTypes": ["janitor"],
+    "minDuration": 1,
+    "maxDuration": 10,
+    "description": "closet",
+    "locations": ["random closet"],
+    "during": ["scheduled", "roaming"],
+    "frequencyPerHour": 0.5 
+  }],
+  "aften": [{ "agents": ["time-of-day"], "location": ["global-state"]}],
+  "formiddag": [{ 
+    "agents": ["time-of-day"], 
+    "location": ["global-state"]
+  }, {
+    "agentTypes": ["student", "teacher", "researcher"],
+    "minDuration": 1,
+    "maxDuration": 10,
+    "description": "toilet",
+    "locations": ["toilet"],
+    "during": ["scheduled", "roaming"],
+    "frequencyPerHour": 0.01
+  }],
+  "frokost": [
+  {
+    "agentTypes": ["student", "teacher", "researcher"],
+    "minDuration": 10,
+    "maxDuration": 30,
+    "description": "lunch",
+    "locations": ["canteen", "canteen", "canteen", "cafe"],
+    "during": ["roaming"],
+    "frequencyPerHour": 4
+  }],
+  "eftermiddag": [{ "agents": ["time-of-day"], "location": ["global-state"]}],
+  "morgen": [{ "agents": ["time-of-day"], "location": ["global-state"]}],
+  "nat": [{ "agents": ["time-of-day"], "location": ["global-state"]}]
+};
+
+function agentNames(name, count) {
+  var result = [];
+  for (var i = 1; i <= count; ++i) {
+    result.push(name + i);
+  }
+  return result;
+}
