69da1c9a651f27ae64bb22c82009e53e95b479cc
UCC Macro <uccmacro@UCCclient1.local>
2015-10-06 10:27:57 +0200

    old changes on ucc-mac

--- data/calendar.js
+++ data/calendar.js
@@ -7,14 +7,14 @@ module.exports = {
   ],
   "workers": [
     {
-      "agents": agentNames("researcher", 10),
+      "agents": agentNames("researcher", 100),
       "activity": "researchers",
-      "locations": ["B.103", "B.105", "B.104", "B.109", "A.104", "A.106"]
+      "locations": ["B.103", "B.105", "B.104", "B.109", "A.104", "A.106", "A.004", "A.101", "A.103", "A.124", "B.101", "C.101", "C.106", "C.107", "C.108", "C.116", "C.117", "C.201", "C.208", "C.226", "C.230", "C.216", "C.220", "A.16A", "A.18A", "A.110A", "A.111A", "A.17D" ]
     },
     {
-      "agents": agentNames("administrator", 10),
+      "agents": agentNames("administrator", 40),
       "activity": "administrators",
-      "locations": ["C.103", "C.105", "C.104", "C.109", "B.104", "B.106"]
+      "locations": ["C.103", "C.105", "C.104", "C.109", "B.104", "B.106", "A.018", "A.021", "A.023Q", "A.025Q", "A.014", "A.027", "A.029", "A.031", "A.019", "A.023U", "A.18H", "A.112H", "A.18J", "A.18L", "A.112J", "A.18N"]
     }
   ],
   "janitors": [
@@ -62,7 +62,7 @@ module.exports = {
     {
       "agentTypes": ["student", "teacher", "researcher"],
       "minDuration": 10,
-      "maxDuration": 30,
+      "maxDuration": 40,
       "description": "lunch",
       "locations": ["canteen", "canteen", "canteen", "cafe"],
       "during": ["roaming"],
